3f41d516ad428a089c71f6fdc3c26a3dfb73373c
Roberta Vieira Sapia <robertavsapia@gmail.com>
2021-02-01 13:51:07 -0300

    iniciando configurações

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,8 +1,8 @@
 export const Register = () => {
-  const rootElement = document.createElement('div');
-  rootElement.innerHTML = `
+    const rootElement = document.createElement('div');
+    rootElement.innerHTML = `
       
   `;
 
-  return rootElement;
+    return rootElement;
 };
