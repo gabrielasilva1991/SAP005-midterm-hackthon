8b1be2ce795f6148c8a0e2f078d37056553a427f
Roberta Vieira Sapia <robertavsapia@gmail.com>
2021-02-02 09:58:01 -0300

    merge1

--- src/pages/toWatchList/index.js
+++ src/pages/toWatchList/index.js
@@ -1,21 +1,8 @@
-import {
-  Navigation
-} from '/pages/navigation/index.js';
-
 export const ToWatchList = () => {
-  const nav = Navigation();
   const rootElement = document.createElement('div');
-
-  rootElement.appendChild(nav);
-
-  const contentElement = () => {
-    const boxElement = document.createElement('div');
-    boxElement.innerHTML = `
-    <h1>Olá Mundo</h1>
+  rootElement.innerHTML = `
+      
   `;
-    return boxElement
-  }
-  rootElement.appendChild(contentElement())
 
   return rootElement;
 };
